2cd21ad6e6b74b959b431416ab98e7923ebd3c02
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-04-14 11:45:47 +0100

    additional simulations

--- src/economy/main.clj
+++ src/economy/main.clj
@@ -3,4 +3,6 @@
 
 (comment
   (run-simulation {:price-adjustment price-adjust-flat})
+  (run-simulation {:price-adjustment price-adjust-dynamic})
+  (run-simulation {:price-adjustment price-adjust-dynamic :fix-prices true :elon-tusk true})
   (stop-server))
